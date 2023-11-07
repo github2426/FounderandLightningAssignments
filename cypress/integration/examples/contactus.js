97d7ff381cbb289df8a38e9382725eea998ce2b2
kumbhar.sachin24@gmail.com <kumbhar.sachin24@gmail.com>
2023-11-07 18:44:46 +0530

    to modified beforeeach, locator and -ve test cases added

--- cypress/integration/examples/contactus.js
+++ cypress/integration/examples/contactus.js
@@ -2,38 +2,38 @@
 
 describe("Assignment fo Test", function() {
 
-    it("Developement of automation contact us", function(){
-
-        //launch contact us page
+    beforeEach(() => {
         cy.visit("https://www.founderandlightning.com/contact")
         cy.get('#hs-eu-cookie-confirmation-inner')
         cy.get('#hs-eu-confirmation-button').click()
-        cy.get('#firstname-b15d4232-7672-429d-81fd-a00020bddf95').type("Sachin");
-        cy.get('#lastname-b15d4232-7672-429d-81fd-a00020bddf95').type("Kumbhar");
-        cy.get('#email-b15d4232-7672-429d-81fd-a00020bddf95').type("kumbhar.sachin24@gmail.com");
-        cy.get('#mobilephone-b15d4232-7672-429d-81fd-a00020bddf95').type("9730490641");
-        cy.get('select').select(1).should('have.value',"Referral");
-        cy.get('select').select(2).should('have.value',"Word of mouth");
-        cy.get('select').select(3).should('have.value',"Event");
-        cy.get('select').select(4).should('have.value',"Article");
-        cy.get('select').select(5).should('have.value',"Facebook");
-        cy.get('select').select(6).should('have.value',"Twitter");
-        cy.get('select').select(7).should('have.value',"Instagram");
-        cy.get('select').select(8).should('have.value',"LinkedIn");
-        cy.get('select').select(9).should('have.value',"Job board");
-        cy.get('select').select(10).should('have.value',"Other");
-        cy.get('select').select(11).should('have.value',"Clubhouse");
-        cy.wait(2000)
-        cy.get('#message-b15d4232-7672-429d-81fd-a00020bddf95').type("need to know next step for appointment")
+    }) 
+
+    it("Developement of automation contact us", function(){
+        //launch contact us page
+        //cy.visit("https://www.founderandlightning.com/contact")
+      
+        cy.get('input[name="firstname"]').type("Sachin")
+        cy.get('input[name="lastname"]').type("Kumbhar")
+        cy.get('input[type="email"]').type("kumbhar.sachin24@gmail.com")
+        cy.get('input[name="mobilephone"]').type("9730490641")
+
+        const page = ['How did you hear about us?','Referral', 'Word of mouth', 'Event','Article','Facebook', 'Twitter','Instagram','LinkedIn','Job board','Other','Clubhouse']
+        let i;
+        for(i=1;i<page.length;i++) {
+            cy.get('select').select(i).should('have.value',page[i]);
+        }
+
+       cy.wait(2000)
+       cy.get('textarea[name="message"]').type("need to know next step for appointment")
         cy.get("input[type='submit']").click()
 
         })
 
         it("verify all links are returns correct result",function(){
             const page = ['Home', 'About Us', 'Our Portfolio','Resources','F+L Capital', 'Careers','FAQs','Tech Glossary','Smoke Test Guide','Top']
-            cy.visit("https://www.founderandlightning.com/contact")
-            cy.get('#hs-eu-cookie-confirmation-inner')
-            cy.get('#hs-eu-confirmation-button').click()
+           // cy.visit("https://www.founderandlightning.com/contact")
+            //cy.get('#hs-eu-cookie-confirmation-inner')
+            //cy.get('#hs-eu-confirmation-button').click()
             page.forEach(page => {
                 cy.contains(page).then((link) => {
                     cy.request(link.prop('href')).its('status').should('eq',200)
@@ -43,14 +43,21 @@ describe("Assignment fo Test", function() {
             cy.get('.social-link-footer').click({ multiple: true });
     
         })
+    
+    it("Verify proper error message asserted on entering a invalid data", function(){
+       // cy.visit("https://www.founderandlightning.com/contact")
+        //cy.get('#hs-eu-cookie-confirmation-inner')
+        //cy.get('#hs-eu-confirmation-button').click()
+        cy.wait(5000)
+        cy.get('input[name="firstname"]').type(" ")
+        cy.get('input[name="lastname"]').type(" ")
+        cy.get('label[class="hs-error-msg hs-main-font-element"]').should("have.text",'Please complete this required field.');
+        cy.get('input[type="email"]').type("s")
+        cy.get('label[class="hs-error-msg hs-main-font-element"]').should("include.text",'Email must be formatted correctly.');
+        cy.get('input[name="mobilephone"]').type('424534')
+        cy.get('label[class="hs-error-msg hs-main-font-element"]').should("include.text",'The number you entered is not in range.');
+        cy.get('textarea[name="message"]').type(" ")
+        cy.get('label[class="hs-error-msg hs-main-font-element"]').should("include.text",'Please complete this required field.');
     })
 
-
-    it("verify assertion for not entering fields givea a proper error message",function(){
-
-        cy.visit("https://www.founderandlightning.com/contact")
-        cy.get('#hs-eu-cookie-confirmation-inner')
-        cy.get('#hs-eu-confirmation-button').click()
-        cy.get('#firstname-b15d4232-7672-429d-81fd-a00020bddf95').type(" ");
-        cy.get('#lastname-b15d4232-7672-429d-81fd-a00020bddf95').type("Kumbhar");
-    })
+})
